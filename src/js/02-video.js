a659b8650c6cbd528da4bc7e52fe0c390c318d2c
Mykhailo Popyzenko <mp.and.sp@ukr.net>
2023-04-19 00:38:14 +0300

    update 2nd and 3rd tasks

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_FORM = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(formInputs, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function formInputs(evt) {
+  const objData = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  localStorage.setItem(FEEDBACK_FORM, JSON.stringify(objData));
+}
+
+const savedObjData = JSON.parse(localStorage.getItem(FEEDBACK_FORM));
+if (savedObjData) {
+  refs.input.value = savedObjData.email;
+  refs.textarea.value = savedObjData.message;
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const objData = {
+    email: refs.input.value,
+    message: refs.textarea.value,
+  };
+  console.log(objData);
+  localStorage.removeItem(FEEDBACK_FORM);
+  evt.currentTarget.reset();
+}
